94ca1435508f39349ca311cf58f5da448d02caef
zaidm124 <zaidm124@gmail.com>
2021-08-28 17:00:17 +0530

    login & register auth

--- src/myComponents/ConfirmedRegistration.js
+++ src/myComponents/ConfirmedRegistration.js
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import db from "../FireBase";
 import "./ConfirmedRegistration.css";
 
 function ConfirmedRegistration() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const submit = () => {
+    if (password === confirmPassword) {
+      db.collection("login")
+        .add({
+          admission: localStorage.getItem("admission"),
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword,
+        })
+        .then(() => {
+          window.location.assign("/profilealumni");
+        });
+    } else {
+      window.alert("Your password is incorrect");
+    }
+  };
   return (
     <div>
       <div class="Background">
         <div className="Confirm">
           <div className="Title">
-            <h2>Hi NAME</h2>
+            <h2>Hi {localStorage.getItem("name")}</h2>
             <h3>Please chose a Password</h3>
           </div>
           <div className="id_password">
             <div>
-              <label>Password</label>
-              <input type="password"></input>
+              <label>Email id</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="text"
+              ></input>
             </div>
             <div>
-              <label>Eamil id</label>
-              <input type="text"></input>
+              <label>Password</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+              ></input>
             </div>
+            <div>
+              <label>Confirm Password</label>
+              <input
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="password"
+              ></input>
+            </div>
+
             <div className="submit_button">
-              <button onClick={() => window.location.assign("/profilealumni")}>
-                search
-              </button>
+              <button onClick={submit}>Register</button>
             </div>
           </div>
         </div>
